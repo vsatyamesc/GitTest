7b5142060d0126cd45fe1b59b4a9f241617e27fc
Satyam <60954779+vsatyamesc@users.noreply.github.com>
2025-03-24 09:15:22 +0530

    Update HrPortalApplication.java

--- Java/Assignment 3/HRPortal/src/main/java/com/satyam/hrportal/HrPortalApplication.java
+++ Java/Assignment 3/HRPortal/src/main/java/com/satyam/hrportal/HrPortalApplication.java
@@ -2,7 +2,7 @@ package com.satyam.hrportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class HrPortalApplication {
